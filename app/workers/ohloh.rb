dad5e5c6d941b59ca30571293abc8ed1853e0be9
Sameer Rahmani <lxsameer@gnu.org>
2014-07-17 02:03:25 +0430

    ohloh worker added

--- app/workers/ohloh.rb
+++ app/workers/ohloh.rb
@@ -1,37 +1,39 @@
 require 'net/http'
 require 'rexml/document'
 
+class OhlohWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
 
-api_key = 'eBziSrPVdYY0FrRpXFqtdg'
-response = Net::HTTP.get_response("www.ohloh.net", "/accounts/lxsameer.xml?v=1&api_key=#{api_key}", 80)
+  recurrence { minutely(Settings.new.ohloh['interval'].to_i) }
+
+  def perform
+    redis = MyRedis.new
+
+    api_key = Secrets.ohloh_key
+    ohloh_user = Settings.new.ohloh['user']
+    response = Net::HTTP.get_response('www.ohloh.net', "/accounts/#{ohloh_user}.xml?v=1&api_key=#{api_key}", 80)
+
+    if response.code != '200'
+      logger.error "[OHLOH] #{response.code} - #{response.message}"
+      return
+    end
+
+    # Parse the response into a structured XML object
+    xml = REXML::Document.new(response.body)
+    # Did Ohloh return an error?
+    error = xml.root.get_elements('/response/error').first
+    if error
+      logger.error "[OHLOH] #{error.text}"
+      return
+    end
 
 
+    xml_kudo_rank = xml.root.get_elements('/response/result/account/kudo_score/kudo_rank').first
+    kudo_rank = xml_kudo_rank ? xml_kudo_rank.text.to_i : 1
+    position = xml.root.get_elements('/response/result/account/kudo_score/position').first.text
 
-if response.code != '200'
-  STDERR.puts "#{response.code} - #{response.message}"
-  exit 1
+    redis.set 'ohloh', JSON.generate({rank: position, level: kudo_rank})
+
+  end
 end
-
-# Parse the response into a structured XML object
-xml = REXML::Document.new(response.body)
-# Did Ohloh return an error?
-error = xml.root.get_elements('/response/error').first
-if error
-  STDERR.puts "#{error.text}"
-  exit 1
-end
-
-# Output all the immediate child properties of an Account
-xml.root.get_elements('/response/result/account').first.each_element do |element|
-  puts "#{element.name}:\t#{element.text}" unless element.has_elements?
-end
-
-xml_kudo_rank = xml.root.get_elements('/response/result/account/kudo_score/kudo_rank').first
-kudo_rank = xml_kudo_rank ? xml_kudo_rank.text.to_i : 1
-position = xml.root.get_elements('/response/result/account/kudo_score/position').first.text
-c = 0
-xml.root.get_elements('/response/result/account/languages/language/total_commits').each do |l|
-  c += l.text.to_i
-end
-
-puts "c<<>>>>>>>>>>>> #{c}"
