50d311930bd6e9a7e9595f71d6c83cfc55f96da3
Sameer Rahmani <lxsameer@gnu.org>
2014-09-09 21:12:57 +0430

    Worker bugs fixed

--- workers.rb
+++ workers.rb
@@ -1,7 +1,9 @@
 require 'sidekiq'
 require 'sidetiq'
 
-require_relative "./app/settings"
+require_relative './app/settings'
+require_relative './app/secrets'
+require_relative './app/lib/redis'
 
 Settings.services.each do |service|
   require_relative "./app/workers/#{service}"
